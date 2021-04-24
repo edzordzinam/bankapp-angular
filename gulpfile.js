c52962dff9094f04b9681098ac2d0d16d1d9d905
Elvis Edzordzinam Dagadu <developer@edzordzinam.me>
2021-04-24 10:50:35 +0000

    .gitignore file update

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var serve = require('gulp-serve');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -35,6 +36,9 @@ gulp.task('install', ['git-check'], function() {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
 });
+
+gulp.task('serve', serve('www'));
+
 
 gulp.task('git-check', function(done) {
   if (!sh.which('git')) {
